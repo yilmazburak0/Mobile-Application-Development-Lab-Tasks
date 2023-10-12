e54da053095874bde9c30a0ba3bf042542d5f8f4
yilmazburak0 <yilmazburak1210@gmail.com>
2023-10-12 23:00:46 +0300

    week2 second commit

--- week2/app/src/main/java/msku/ceng/madlab/week2/MainActivity.java
+++ week2/app/src/main/java/msku/ceng/madlab/week2/MainActivity.java
@@ -2,13 +2,38 @@ package msku.ceng.madlab.week2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnArrayAdapter;
 
+    Button btnCustomAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnArrayAdapter = (Button) findViewById(R.id.buttonArrayAdapter);
+        btnArrayAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ArrayAdapterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCustomAdapter = findViewById(R.id.buttonCustomAdapter);
+
+        btnCustomAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CustomAdapterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
