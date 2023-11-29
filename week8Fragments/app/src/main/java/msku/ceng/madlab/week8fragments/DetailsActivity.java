3025d1e28cc0abd3cf2058d4d7b19ac462d448b8
yilmazburak0 <yilmazburak1210@gmail.com>
2023-11-29 19:16:36 +0300

    week8 Fragments commit

---
+++ week8Fragments/app/src/main/java/msku/ceng/madlab/week8fragments/DetailsActivity.java
@@ -0,0 +1,21 @@
+package msku.ceng.madlab.week8fragments;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        Movie movie = (Movie) getIntent().getSerializableExtra("movie");
+        FragmentTransaction fts = getSupportFragmentManager().beginTransaction();
+        DetailsFragment df = DetailsFragment.newInstance(movie);
+        fts.add(R.id.container, df);
+        fts.commit();
+    }
+}
