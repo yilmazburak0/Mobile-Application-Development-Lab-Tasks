6c3b5578f45eb95b1fdd9c32e8c14a5af2d68b8f
yilmazburak0 <yilmazburak1210@gmail.com>
2023-12-24 11:30:29 +0300

    week11(myNotes)

---
+++ MyNotes/app/src/main/java/msku/ceng/madlab/mynotes/MyNoteRecyclerViewAdapter.java
@@ -0,0 +1,79 @@
+package msku.ceng.madlab.mynotes;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import msku.ceng.madlab.mynotes.placeholder.PlaceholderContent.PlaceholderItem;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyNoteRecyclerViewAdapter extends RecyclerView.Adapter<MyNoteRecyclerViewAdapter.ViewHolder> {
+    private ArrayList<Note> notes;
+    private NoteFragment.OnNoteListInteractionListener listener;
+
+    public MyNoteRecyclerViewAdapter(ArrayList<Note> notes, NoteFragment.OnNoteListInteractionListener listener) {
+        this.notes = notes;
+        this.listener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_note, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = notes.get(position);
+        holder.mHeaderView.setText(notes.get(position).getHeader());
+        holder.mDateView.setText(new SimpleDateFormat("yyyy-MM-dd").format(notes.get(position).getDate()));
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null)
+                {
+                    listener.onNoteSelected(holder.mItem);
+                }
+            }
+        });
+
+        if (position % 2 == 1)
+        {
+            holder.mView.setBackgroundColor(Color.CYAN);
+        }else
+        {
+            holder.mView.setBackgroundColor(Color.BLUE);
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mHeaderView;
+        public final TextView mDateView;
+        public final View mView;
+        public Note mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mHeaderView = view.findViewById(R.id.note_header);
+            mDateView = view.findViewById(R.id.note_date);
+            mView = view;
+        }
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mHeaderView.getText() + "'";
+        }
+    }
+}
